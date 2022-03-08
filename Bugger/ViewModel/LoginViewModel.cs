884cbfa12ee187f67a5f0a2a93b6444679d58bcc
roymw09 <60860251+roymw09@users.noreply.github.com>
2022-03-08 14:48:00 -0330

    Added LoginView and LoginViewModel

---
+++ Bugger/ViewModel/LoginViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Bugger.Command;
+using Bugger.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Bugger.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string hostName;
+        public string HostName
+        {
+            get
+            {
+                return hostName;
+            }
+            set
+            {
+                hostName = value;
+                onPropertyChanged(nameof(HostName));
+            }
+        }
+
+        private string databaseName;
+        public String DatabaseName
+        {
+            get
+            {
+                return databaseName;
+            }
+            set
+            {
+                databaseName = value;
+                onPropertyChanged(nameof(DatabaseName));
+            }
+        }
+
+        private string username;
+        public String Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                onPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string password;
+        public String Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                onPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public LoginViewModel(NavigationService loginViewNavigationService)
+        {
+            LoginCommand = new NavigateCommand(loginViewNavigationService);
+        }
+    }
+}
